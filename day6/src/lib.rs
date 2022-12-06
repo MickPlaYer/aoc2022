70ad0545e7c5f6c9983d9d006d965e582c849d97
mickplayer <mickgamehome@gmail.com>
2022-12-07 01:16:31 +0800

    day6 part2

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,26 +1,25 @@
 pub fn process_part1(content: String) -> Option<usize> {
-    const SIGN_LENGTH: usize = 4;
-    for i in 0..(content.len() - SIGN_LENGTH) {
-        let chars = content
-            .chars()
-            .skip(i)
-            .take(SIGN_LENGTH)
-            .collect::<Vec<char>>();
+    find_signal_with_length(content, 4)
+}
+
+pub fn process_part2(content: String) -> Option<usize> {
+    find_signal_with_length(content, 14)
+}
+
+fn find_signal_with_length(content: String, length: usize) -> Option<usize> {
+    for i in 0..(content.len() - length) {
+        let chars = content.chars().skip(i).take(length).collect::<Vec<char>>();
         let mut is_all_uniq = true;
-        for x in 0..SIGN_LENGTH {
-            for y in (x + 1)..SIGN_LENGTH {
+        for x in 0..length {
+            for y in (x + 1)..length {
                 is_all_uniq = is_all_uniq && chars[x] != chars[y];
             }
         }
         if is_all_uniq {
-            return Some(i + SIGN_LENGTH);
+            return Some(i + length);
         }
     }
     None
-}
-
-pub fn process_part2(content: String) -> Option<usize> {
-    Some(0)
 }
 
 #[cfg(test)]
@@ -47,10 +46,20 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "for now"]
     fn process_part2_with_sample() {
         let content = read_sample(6);
-        let result = process_part2(content);
-        assert_eq!(result, None);
+        let expects = vec![Some(19), Some(23), Some(23), Some(29), Some(26)];
+        let answers = content
+            .lines()
+            .map(|line| process_part2(line.into()))
+            .collect::<Vec<Option<usize>>>();
+        assert_eq!(expects, answers);
+    }
+
+    #[test]
+    fn process_part2_with_input() {
+        let content = read_input(6);
+        let answer = process_part2(content);
+        assert_eq!(Some(2773), answer);
     }
 }
