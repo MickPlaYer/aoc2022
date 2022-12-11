9ee484c05e6205807c90d1693c67b8441fa46095
mickplayer <mickgamehome@gmail.com>
2022-12-11 10:38:18 +0800

    day10 part2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -42,18 +42,18 @@ impl Cpu {
         }
     }
 
-    fn run_and_monitor_interesting_signal_strengths(&mut self) -> isize {
+    fn run<F>(&mut self, mut monitor_handler: F)
+    where
+        F: FnMut(isize, isize),
+    {
         let commands = &mut self.commands;
         commands.reverse();
         let mut last_command = commands.pop().unwrap();
         let mut command_time = last_command.get_time();
-        let mut result = 0;
         loop {
             self.cycle_number += 1;
             command_time -= 1;
-            if self.cycle_number % 40 == 20 {
-                result += self.cycle_number * self.register
-            }
+            monitor_handler(self.cycle_number, self.register);
             if command_time == 0 {
                 if let Command::Addx(value) = last_command {
                     self.register += value
@@ -67,6 +67,15 @@ impl Cpu {
             last_command = commands.pop().unwrap();
             command_time = last_command.get_time();
         }
+    }
+
+    fn run_and_monitor_interesting_signal_strengths(&mut self) -> isize {
+        let mut result = 0;
+        self.run(|cycle_number, register| {
+            if cycle_number % 40 == 20 {
+                result += cycle_number * register
+            }
+        });
         result
     }
 }
@@ -78,8 +87,23 @@ pub fn process_part1(content: String) -> Option<isize> {
     Some(result)
 }
 
-pub fn process_part2(content: String) -> Option<usize> {
-    None
+pub fn process_part2(content: String) -> String {
+    let commands = content.lines().map(|line| Command::parse(line)).collect();
+    let mut cpu = Cpu::new(commands);
+    let mut screen = Vec::new();
+    cpu.run(|cycle_number, register| {
+        if screen.len() >= 240 {
+            return;
+        }
+        let crt_column = (cycle_number - 1) % 40;
+        let sprite_range = register - 1..=register + 1;
+        if sprite_range.contains(&crt_column) {
+            screen.push("#")
+        } else {
+            screen.push(".")
+        }
+    });
+    screen.join("")
 }
 
 #[cfg(test)]
@@ -106,13 +130,29 @@ mod tests {
     fn process_part2_with_sample() {
         let content = read_sample(DAY_NUMBER);
         let answer = process_part2(content);
-        assert_eq!(Some(0), answer);
+        let expected = "\
+            ##..##..##..##..##..##..##..##..##..##..\
+            ###...###...###...###...###...###...###.\
+            ####....####....####....####....####....\
+            #####.....#####.....#####.....#####.....\
+            ######......######......######......####\
+            #######.......#######.......#######.....\
+        ";
+        assert_eq!(expected, answer);
     }
 
     #[test]
     fn process_part2_with_input() {
         let content = read_input(DAY_NUMBER);
         let answer = process_part2(content);
-        assert_eq!(Some(0), answer);
+        let expected = "\
+            ####.#..#.###..#..#.####.###..#..#.####.\
+            #....#.#..#..#.#..#.#....#..#.#..#....#.\
+            ###..##...#..#.####.###..#..#.#..#...#..\
+            #....#.#..###..#..#.#....###..#..#..#...\
+            #....#.#..#.#..#..#.#....#....#..#.#....\
+            ####.#..#.#..#.#..#.####.#.....##..####.\
+        ";
+        assert_eq!(expected, answer);
     }
 }
